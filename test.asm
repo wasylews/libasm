1d6c9c3efc4b9e996d18b1fca4a129b0667b3664
Wasylews <ua.wasylews@gmail.com>
2016-05-01 20:49:44 +0300

    Fix fgets() file handle corruption

--- test.asm
+++ test.asm
@@ -4,8 +4,9 @@
 .stack 256h
 
 .data
-
-s db 'hello world', 0
+filename db 'test.asm', 0
+handle dw ?
+buffer db 20 dup('')
 
 .code
 
@@ -13,12 +14,38 @@ LOCALS
 
 include stdio.inc
 
-main proc
+main proc c
     mov ax, @data
     mov ds, ax
 
-    puts s
+    push RO
+    push offset filename
+    call fopen
+    add sp, 4
 
+    test ax, ax
+    je @@exit
+
+    mov handle, ax
+@@read_loop:
+    push handle
+    push 20
+    push offset buffer
+    call fgets
+    add sp, 6
+
+    test ax, ax
+    je @@close_file
+
+    puts buffer
+    jmp @@read_loop
+
+@@close_file:
+    push handle
+    call fclose
+    add sp, 2
+
+@@exit:
     mov ax, 4c00h
     int 21h
 
